8d4aacb6829ce56c377e2d8094cee4765333cd64
Philippe Cartier <cartier.philippe@gmail.com>
2012-01-29 12:48:01 +0100

    Offre du cinema educateur: recherche se fait sur plusieurs champs

--- app/models/office.rb
+++ app/models/office.rb
@@ -2,7 +2,7 @@ class Office < ActiveRecord::Base
   
   def self.search(search)
     if search
-      where('titre LIKE ?', "%#{search}%")
+      where('titre ILIKE ? OR titreo ILIKE ? OR rea ILIKE ? OR year ILIKE ? OR dure ILIKE ? OR note ILIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
     else
       scoped
     end
