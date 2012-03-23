ce61b20b53a9b8d54341e4619d835e39a7059029
Philippe Cartier <cartier.philippe@gmail.com>
2012-03-23 09:59:53 +0100

    adding pluck (back porting)

--- app/models/function.rb
+++ app/models/function.rb
@@ -4,5 +4,14 @@ class Function < ActiveRecord::Base
   has_many :people, :dependent => :destroy
   accepts_nested_attributes_for :people, :reject_if => :all_blank,  :allow_destroy => true
    
+  def self.search(search)
+    if (search)
+      where 'LOWER(name) LIKE :search', 
+            { :search => "%#{search.downcase}%" }
+    else
+      scoped
+    end
+  end
+
    
 end
