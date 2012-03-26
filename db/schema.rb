c52271f1c252dd9917811be5fe8e4b6ab01d2e4c
Philippe Cartier <cartier.philippe@gmail.com>
2012-03-26 07:55:10 +0200

    finished the migration of all the joint tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120326054930) do
+ActiveRecord::Schema.define(:version => 20120326055138) do
 
   create_table "cinematographers", :force => true do |t|
     t.string   "name"
