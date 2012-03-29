b036551c124210488d210ef9007f8dc20ec8fb2c
Philippe Cartier <cartier.philippe@gmail.com>
2012-03-29 07:49:29 +0200

    fixing the _form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,11 +50,11 @@ module ApplicationHelper
   end
   
   
-  
-  def link_to_add_fields(name, f, association)
+  #localvariables: example: { :forced_ville_id => 2 } #used for joint_fields partial for now 
+  def link_to_add_fields(name, f, association, localvariables = {})
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_fields", :f => builder)
+      render :partial => association.to_s.singularize + "_fields", :locals => localvariables.merge( {:f => builder} )
     end
     link_to_function(name, ("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
   end
