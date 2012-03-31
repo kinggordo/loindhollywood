16cf1abb42d818259e1978252db8ce73e942d980
Philippe Cartier <cartier.philippe@gmail.com>
2012-03-31 07:02:46 +0200

    completed the search on salle

--- app/models/salleinfo.rb
+++ app/models/salleinfo.rb
@@ -4,4 +4,14 @@ class Salleinfo < ActiveRecord::Base
   
   has_many :salleinfoattribs, :dependent => :destroy
   accepts_nested_attributes_for :salleinfoattribs, :reject_if => :all_blank,  :allow_destroy => true
+
+  def self.search(search)
+    if (search)
+      where 'LOWER(name) LIKE :search',
+      { :search => "%#{search.downcase}%" }
+    else
+      scoped
+    end
+  end
+
 end
