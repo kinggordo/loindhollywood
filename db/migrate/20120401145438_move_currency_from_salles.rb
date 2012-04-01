dbffdabd6b015cad710497bb46979d3a4bf9c4a8
Philippe Cartier <cartier.philippe@gmail.com>
2012-04-01 16:56:05 +0200

    moved the values around

---
+++ db/migrate/20120401145438_move_currency_from_salles.rb
@@ -0,0 +1,15 @@
+class MoveCurrencyFromSalles < ActiveRecord::Migration
+  def self.up
+    say_with_time "moving currency_id and number into own table" do
+      Salle.all.each do |j|
+        if !j.currency_id.nil? && !j.number.blank?
+          j.prices.create! :currency_id => j.currency_id, :number => j.number
+        end
+      end
+    end
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120401142644) do
+ActiveRecord::Schema.define(:version => 20120401145438) do
 
   create_table "cinematographers", :force => true do |t|
     t.string   "name"
