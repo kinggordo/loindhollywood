5b19bce8ba46e49323d9c4d31ea8871745833b2f
Philippe Cartier <cartier.philippe@gmail.com>
2012-04-12 10:40:57 +0200

    fixed the login

--- config/routes.rb
+++ config/routes.rb
@@ -97,10 +97,10 @@ Kinggordo::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "Home#index"
-  
-  namespace :user do
-  root :to => "films#index"
-  end
+  match 'user' => 'films#index'
+  #namespace :user do
+  #  root :to => "films#index"
+  #end
 
   # See how all your routes lay out with "rake routes"
 
