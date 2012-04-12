80c097346423792d96b3c51027ff9978d8a8446a
Philippe Cartier <cartier.philippe@gmail.com>
2012-04-12 11:28:18 +0200

    search is case insensitive on offices

--- app/models/office.rb
+++ app/models/office.rb
@@ -2,7 +2,8 @@ class Office < ActiveRecord::Base
   
   def self.search(search)
     if search
-      where('titre LIKE ? OR titreo LIKE ? OR rea LIKE ? OR year LIKE ? OR dure LIKE ? OR note LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+      where('LOWER(titre) LIKE :search OR LOWER(titreo) LIKE :search OR LOWER(rea) LIKE :search OR LOWER(year) LIKE :search OR LOWER(country) LIKE :search OR LOWER(dure) LIKE :search OR LOWER(son) LIKE :search OR LOWER(producer) LIKE :search OR LOWER(lieu) LIKE :search OR LOWER(format) LIKE :search OR LOWER(support) LIKE :search OR LOWER(genre) LIKE :search OR LOWER(themea) LIKE :search OR LOWER(themeb) LIKE :search OR LOWER(note) LIKE :search', 
+            { :search => "%#{search}%" })
     else
       scoped
     end
